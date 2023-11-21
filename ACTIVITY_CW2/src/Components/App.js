5567386b9669445bd2e2b87829fe9918df5b69ab
prasadsenapathy28 <117511282+prasadsenapathy28@users.noreply.github.com>
2023-11-21 10:11:23 +0530

    Add files via upload

---
+++ ACTIVITY_CW2/src/Components/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div className="maindiv">
+      <h1 style={{color: 'green'}}>Inline Style in JSX Example</h1>
+      <div className="insidediv">
+        <p className="para">Hitler was born in Braunau am Inn in Austria-Hungary and was raised near Linz. He lived in Vienna later in the first decade of the 1900s before moving to Germany in 1913. He was decorated during his service in the German Army in World War I. In 1919, he joined the German Workers' Party (DAP), the precursor of the Nazi Party, and in 1921 was appointed leader of the Nazi Party. In 1923, he attempted to seize governmental power in a failed coup in Munich and was imprisoned with a sentence of five years. In prison, he dictated the first volume of his autobiography and political manifesto Mein Kampf ("My Struggle"). After his early release in 1924, Hitler gained popular support by attacking the Treaty of Versailles and promoting pan-Germanism, anti-Semitism and anti-communism with charismatic oratory and Nazi propaganda. He frequently denounced international capitalism and communism as part of a Jewish conspiracy.</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
